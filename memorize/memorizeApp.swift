e7005d104c3dc4cbf3f782b175d213922dc9dafe
Hywre Cesar <hcbp@cin.ufpe.br>
2021-12-04 14:16:13 -0300

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by hcbp on 04/12/21.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
