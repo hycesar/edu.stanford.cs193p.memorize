2b4136f8b54c1da1313abe30f5c1b2ac3e056d2c
Hywre Cesar <hcbp@cin.ufpe.br>
2021-12-05 15:22:08 -0300

    init

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack  {
-            CardView()
+            CardView() \
             CardView()
             CardView()
             CardView()
