d46e58cb082b19c2aa4c57b8acaa383b5e229d4c
Hywre Cesar <hcbp@cin.ufpe.br>
2021-12-05 14:54:08 -0300

    initial

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -9,8 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack  {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        .padding()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        ZStack (alignment: .center) {
+            RoundedRectangle(cornerRadius: 20.0)
+                .stroke(lineWidth: 3)
+                .fill(.red)
+                .colorInvert()
+            Text("Hello, my worlds!")
+        }
     }
 }
 
